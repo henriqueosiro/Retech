7edaeb6b8d2ddaad150a0c9e87b850169d4bd957
Henrique Osiro <henriqueosiro@hotmail.com>
2019-10-20 12:50:24 -0300

    Criar a estrutura da base.html

--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Cadastro(models.Model):
+    email = models.EmailField(max_length=255, verbose_name='Email', unique=True)
+    nome = models.CharField(max_length=255, verbose_name='Nome')
+    senha = models.CharField(max_length=16, verbose_name='Senha')
+
+class Doacao(models.Model):
+    cep = models.CharField(min_length=8, max_length=8, verbose_name='CEP')
+    criado_em =  models.DateTimeField(default=timezone.now)
+    descricao = models.TextField()
+    equipamento = models.CharField(max_length=255, verbose_name='Equipamento')
