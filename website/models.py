73aa43c9eb72384ce9f9f9054061d5288e54226f
Henrique Osiro <henriqueosiro@hotmail.com>
2019-10-22 21:13:14 -0300

    Criar objetos e adicionar as propriedades com escolha do formulário

--- website/models.py
+++ website/models.py
@@ -2,14 +2,34 @@ from django.db import models
 from django.utils import timezone
 
 class Cadastro(models.Model):
+
+    NOVIDADES = (
+        ('s', 'Sim')
+        ('n', 'Não')
+    )
+
+    REGIAO = (
+        ('p.1', 'Zona Norte')
+        ('p.2', 'Zona Sul')
+        ('p.3', 'Zona Leste')
+        ('p.4', 'Zona Oeste')
+    )
+    SEXO = (
+        ('m', 'Masculino')
+        ('f', 'Feminino')
+        ('n', 'None')
+    )
+
     criado_em = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(verbose_name='Descrição do Equipamento')
     email = models.EmailField(max_length=255, verbose_name='Email', unique=True)
-    equipamento = models.CharField(max_length=255, verbose_name='Nome do Equipamento')
     foto = models.ImageField(verbose_name='Foto do Equipamento')
     nascimento = models.DateField(verbose_name='Data de Nascimento')
     nome = models.CharField(max_length=255, verbose_name='Nome')
+    novidades = models.CharField(choices=NOVIDADES, verbose_name='Novidades')
+    regiao = models.CharField(choices=REGIAO, verbose_name='Região')
     senha = models.CharField(min_length=8, max_length=16, verbose_name='Senha')
+    sexo = models.CharField(choices=SEXO, verbose_name='Sexo')
     telefone = models.IntegerField(min_length=11, max_length=11, verbose_name='Telefone')
 
 class Contato(models.Model):
