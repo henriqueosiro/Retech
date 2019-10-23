21d1f59d09afe9d70af9c073dedaad23370daa2b
Henrique Osiro <henriqueosiro@hotmail.com>
2019-10-23 17:53:29 -0300

    Configurar as urls

--- retech/urls.py
+++ retech/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from website import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('coachs/cadastro', views.cadastro),
+    path('coachs/contato', views.contato),
 ]
